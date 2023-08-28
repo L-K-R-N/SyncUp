073837174629d9114b2b037327cd99b96864e7aa
L K R N <artemabramcuk45@gmail.com>
2023-08-28 15:27:58 +0300

    added accordion and tabs

--- markup/static/js/plugins/main.js
+++ markup/static/js/plugins/main.js
@@ -31,3 +31,61 @@ if (document.querySelector(".custom-select-field")) {
 		}
 	})
 }
+
+
+// Tabs
+
+// При нажатии на заголовок таба, считывается значение его атрибута data-tab. 
+// Далее ему и блоку с соответствующим значением атрибута 'data-tab' присваивается класс 'active-tab', а всем остальным блокам и заголовкам, у которых есть такой атрибут, класс 'active-tab' снимается
+
+
+const tabPlugin = (header) => {
+	
+	header.addEventListener('click', (e) => {
+		const target = e.target.closest('.prices-tabs__container-title');
+		if (!target) return;
+
+		
+
+		for (let elem of document.querySelectorAll('[data-tab]')) {
+			elem.classList.remove('active-tab');
+
+			if (elem.getAttribute('data-tab') === target.getAttribute('data-tab')) {
+				elem.classList.add('active-tab')
+				target.classList.add('active-tab')
+			}
+		}
+	})
+}
+
+tabPlugin(document.querySelector('.prices-tabs__header'));
+
+// При нажатии на блок с вопросами, проверяем нажатие на хедер.
+// Если у хедера есть класс 'active-accordion', убираем этот класс.
+// Если класса нет, убираем его у всех других блоков и добавляем тому, на который нажали
+
+const accordion = (cards) => {
+
+	cards.addEventListener('click', (e) => {
+		const target = e.target.closest('.questions__card-header')
+
+		if (!target) return;
+
+		const targetContent = document.getElementById(target.dataset.accordion)
+		
+		if (targetContent.classList.contains('active-accordion')) {
+			targetContent.classList.remove('active-accordion');
+			targetContent.style.height = 0;
+
+		} else {
+			for (let elem of document.querySelectorAll('.questions__card-answer')) {
+				elem.classList.remove('active-accordion')
+				elem.style.height = 0;
+			}
+			targetContent.classList.add('active-accordion')
+			targetContent.style.height = targetContent.scrollHeight + 'px'
+		}
+	})
+}
+
+accordion(document.querySelector('.questions__cards'))
